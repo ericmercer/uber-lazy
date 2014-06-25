f207bdba738dab49d51ebfe5d72c3a6a8faaa11e
Eric Mercer <eric.mercer@byu.edu>
2014-06-25 11:05:34 -0700

    Added another example:

---
+++ examples/SimpleExample/SimpleExample.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+(require redex/reduction-semantics)
+(provide (all-defined-out))
+
+(define SimpleExample
+  (term (class SimpleExample
+          (
+           [T a]
+           [T b]
+          )
+          
+          (
+           (T run () (if ((this $ a) == (this $ b))
+                         (this $ a)
+                      else
+                         (this $ b)
+                      ))
+          )
+          )
+))
